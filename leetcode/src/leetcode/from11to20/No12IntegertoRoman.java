df0b5f3b9171ff58310851033ed6a6d46f7de446
shuochijin <835391854@163.com>
2019-04-04 15:10:41 +0800

    Update No12IntegertoRoman.java

--- leetcode/src/leetcode/from11to20/No12IntegertoRoman.java
+++ leetcode/src/leetcode/from11to20/No12IntegertoRoman.java
@@ -47,4 +47,43 @@ public class No12IntegertoRoman {
 		}
 		return result;
 	}
+	
+	public String intToRoman2(int num) {
+		StringBuilder result = new StringBuilder();
+		char[] syb = new char[] {'I', 'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+		int[] bits = new int[] {1, 1, 5, 10, 50, 100, 500, 1000};
+		int r = num / bits[syb.length - 1];
+		int t = num % bits[syb.length - 1];
+		for(int j = 0; j < num / bits[syb.length - 1]; ++ j) {
+			result.append(syb[syb.length - 1]);
+		}
+		r = t / bits[syb.length - 2];
+		t = t % bits[syb.length - 1];
+		
+		for(int i = syb.length - 2; i > 0; -- i) {
+			r = t / bits[i];
+			if(r != 0) {
+				if(i % 2 == 0) {
+					if(t / bits[i - 1] == 9) {
+						result.append(syb[i - 1]);
+						result.append(syb[i + 1]);
+						i --;
+					}else {
+						result.append(syb[i]);
+					}
+				}else {
+					if(t / bits[i] == 4) {
+						result.append(syb[i]);
+						result.append(syb[i + 1]);
+					}else {
+						for(int j = 0; j < t / bits[i]; ++ j) {
+							result.append(syb[i]);
+						}
+					}
+				}
+			}
+			t = t % bits[i];
+		}
+		return result.toString();
+	}
 }
