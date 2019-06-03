008a8940294b7cf55af7c0a7b6c9779e13745c56
shuochijin <835391854@163.com>
2019-06-03 17:59:57 +0800

    Update No10RegularExpressionMatching.java

--- leetcode/src/leetcode/from1to10/No10RegularExpressionMatching.java
+++ leetcode/src/leetcode/from1to10/No10RegularExpressionMatching.java
@@ -17,64 +17,8 @@ public class No10RegularExpressionMatching {
 	
 	public boolean isMatch(String s, String p) {
 		boolean result = false;
-		List<String> array = new ArrayList<String>();
-		char[] pchars = p.toCharArray();
-		for(int i = 0; i < pchars.length; ++ i) {
-			if(pchars[i] == '*') {
-				char temp = pchars[i];
-				pchars[i] = pchars[i - 1];
-				pchars[i - 1] = temp;
-			}
-		}
-		p = new String(pchars);
-		for(int front = 0, tail = front, i = 0; i < p.length(); ++ i) {
-			if(p.charAt(i) == '*') {
-				if(front != tail) {
-					array.add(p.substring(front, tail));
-				}
-				front = i;
-				tail = i + 2;
-				array.add(p.substring(front, tail));
-				front = i + 2;
-				tail = front;
-			}else {
-				tail ++;
-				if(tail == p.length()) {
-					array.add(p.substring(front, tail));
-				}
-			}
-		}
 		
-		Stack<Integer> stack = new Stack<Integer>();
-		int point = 0;
-		for(int i = 0; i < array.size(); ++ i) {
-			String node = array.get(i);
-			if(node.equals("*.")) {
-				if(stack.get(i) == null) {
-					stack.add(point);
-				}else {
-					point = stack.pop();
-					point ++;
-					stack.push(point);
-				}
-			}else if(node.startsWith("*")) {
-				char c = node.charAt(1);
-				point = stack.pop();
-				point ++;
-				if(s.charAt(point) != c) {
-					break;
-				}
-				stack.push(point);
-			}else {
-				
-			}
-			if(point == pchars.length) {
-				break;
-			}
-			if(i == array.size() - 1 && point == pchars.length) {
-				result = true;
-			}
-		}
+		for(;;)
 		
 		return result;
 	}
