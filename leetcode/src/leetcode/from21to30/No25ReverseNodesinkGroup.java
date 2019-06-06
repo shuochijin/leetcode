d15c6e1e74e024bc53c906902ddaa3cd87c753ce
shuochijin <835391854@163.com>
2019-06-06 17:48:56 +0800

    Create No25ReverseNodesinkGroup.java

---
+++ leetcode/src/leetcode/from21to30/No25ReverseNodesinkGroup.java
@@ -0,0 +1,60 @@
+package leetcode.from21to30;
+
+import util.ListNode;
+
+public class No25ReverseNodesinkGroup {
+	
+	public static void main(String[] args) {
+		No25ReverseNodesinkGroup n = new No25ReverseNodesinkGroup();
+		int[] nums = new int[] {2,3,4,5};
+		
+		ListNode head = new ListNode(1);
+		ListNode p = head;
+		for(int j = 0; j < nums.length; ++ j) {
+			p.next = new ListNode(nums[j]);
+			p = p.next;
+		}
+		ListNode s = n.reverseKGroup(head, 3);
+		while(s != null) {
+			System.out.print(s.val + " ");
+			s = s.next;
+		}
+	}
+	
+	public ListNode reverseKGroup(ListNode head, int k) {
+		ListNode result = new ListNode(-1);
+		
+		ListNode p = result;
+		ListNode temp;
+		int c;
+		a:while(head != null) {
+			c = 0;
+			temp = head;
+			while(c < k - 1) {
+				if(temp.next == null) {
+					while(head != null) {
+						p.next = head;
+						head = head.next;
+						p = p.next;
+					}
+					break a;
+				}
+				temp = temp.next;
+				c ++;
+			}
+			c = 0;
+			while(c < k && head != null) {
+				temp = head;
+				head = head.next;
+				temp.next = p.next;
+				p.next = temp;
+				c ++;
+			}
+			while(p.next != null) {
+				p = p.next;
+			}
+		}
+		
+		return result.next;
+	}
+}
