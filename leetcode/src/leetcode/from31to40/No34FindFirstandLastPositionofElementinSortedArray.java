f2cd6e371264d908e638b0842334ee8d6db26936
shuochijin <835391854@163.com>
2019-06-21 17:37:53 +0800

    Update No34FindFirstandLastPositionofElementinSortedArray.java

--- leetcode/src/leetcode/from31to40/No34FindFirstandLastPositionofElementinSortedArray.java
+++ leetcode/src/leetcode/from31to40/No34FindFirstandLastPositionofElementinSortedArray.java
@@ -6,7 +6,9 @@ public class No34FindFirstandLastPositionofElementinSortedArray {
 	
 	public static void main(String[] args) {
 		No34FindFirstandLastPositionofElementinSortedArray n = new No34FindFirstandLastPositionofElementinSortedArray();
-		System.out.println(Arrays.toString(n.searchRange(new int[] {}, 0)));
+		System.out.println(Arrays.toString(n.searchRange(new int[] {5,7,7,8,8,10}, 8)));
+		System.out.println(Arrays.toString(n.searchRange(new int[] {5,7,7,8,8,10}, 6)));
+		System.out.println(Arrays.toString(n.searchRange(new int[] {2,2}, 2)));
 	}
 	
 	public int[] searchRange(int[] nums, int target) {
@@ -17,8 +19,30 @@ public class No34FindFirstandLastPositionofElementinSortedArray {
 		while(front <= tail) {
 			if(target < nums[(front + tail) / 2]) {
 				tail = (front + tail) / 2 - 1;
-			} else {
+			} else if(target > nums[(front + tail) / 2]) {
 				front = (front + tail) / 2 + 1;
+			} else {
+				result[0] = (front + tail) / 2;
+				result[1] = (front + tail) / 2;
+				break;
+			}
+		}
+		
+		front = 0;
+		while(front < result[0]) {
+			if(nums[(front + result[0]) / 2] != target) {
+				front = (front + result[0]) / 2 + 1;
+			} else {
+				result[0] = (front + result[0]) / 2;
+			}
+		}
+		
+		tail = nums.length - 1;
+		while(result[1] < tail) {
+			if(nums[(result[1] + tail + 1) / 2] == target) {
+				result[1] = (result[1] + tail + 1) / 2;
+			} else {
+				tail = (result[1] + tail + 1) / 2 - 1;
 			}
 		}
 		
