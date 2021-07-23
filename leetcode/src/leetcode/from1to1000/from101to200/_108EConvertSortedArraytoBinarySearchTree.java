69d6e831b3a11a6cba8264ff1f26bc5970774c1f
shuochijin <835391854@163.com>
2021-07-23 17:53:28 +0800

    Update _108EConvertSortedArraytoBinarySearchTree.java

--- leetcode/src/leetcode/from1to1000/from101to200/_108EConvertSortedArraytoBinarySearchTree.java
+++ leetcode/src/leetcode/from1to1000/from101to200/_108EConvertSortedArraytoBinarySearchTree.java
@@ -6,10 +6,10 @@ public class _108EConvertSortedArraytoBinarySearchTree {
 	
 	public static void main(String[] args) {
 		
-		int i = 8;
+		int i = 0;
 		
-		while(i > 0) {
-			i = i >> 1;
+		while(i < 100) {
+			i = i << 1;
 			System.out.println(i);
 		}
 		
@@ -19,6 +19,18 @@ public class _108EConvertSortedArraytoBinarySearchTree {
 	
 	public TreeNode sortedArrayToBST(int[] list) {
 		TreeNode root = null;
+		int i = list.length;
+		int count = 1;
+		i = i >> 1;
+		while(i > 0) {
+			i = i >> 1;
+			count =  count << 1;
+		}
+		if(count < list.length) {
+			count = count << 1;
+		}
+		
+		Integer[] is = new Integer[count];
 		return root;
 	}
 }
