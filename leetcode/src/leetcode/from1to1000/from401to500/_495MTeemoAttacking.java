754a177a67ab250cf5148f8acd468bda628c1463
shuochijin <835391854@163.com>
2021-10-19 17:44:39 +0800

    20211019

--- leetcode/src/leetcode/from1to1000/from401to500/_495MTeemoAttacking.java
+++ leetcode/src/leetcode/from1to1000/from401to500/_495MTeemoAttacking.java
@@ -3,9 +3,14 @@ package leetcode.from1to1000.from401to500;
 public class _495MTeemoAttacking {
 	
 	public static void main(String[] args) {
-		_495MTeemoAttacking n = new _495MTeemoAttacking();
-		System.out.println(n.findPoisonedDuration(new int[] {1,4}, 2));
-		System.out.println(n.findPoisonedDuration(new int[] {1,2}, 2));
+		String s = "1..2.3.4.5.6.7";
+		String[] ss = s.split("[.]");
+		for(int i = 0; i < ss.length; ++ i) {
+			System.out.println(ss[i]);
+		}
+//		_495MTeemoAttacking n = new _495MTeemoAttacking();
+//		System.out.println(n.findPoisonedDuration(new int[] {1,4}, 2));
+//		System.out.println(n.findPoisonedDuration(new int[] {1,2}, 2));
 	}
 	
 	public int findPoisonedDuration(int[] timeSeries, int duration) {
